243aaceae63d98838d145075f6d5031f551cb22b
Jeff Handa <jeffrey.handa@liferay.com>
2022-11-29 14:11:38 -0500

    Adding barebones rule

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ you have a custom field defined as a boolean with a key of on-hold.
 
 There are multiple branches so you can see the progression of the renderers
 1. [Starting Point](https://github.com/jhanda/demo-order-rule/tree/1-starting-point)
-2. [Barebones Order Rule]()
+2. [Barebones Order Rule](https://github.com/jhanda/demo-order-rule/tree/2-barebones-rule)
 
 
 Developed for the [Demystifying Liferay Commerce Extensions](https://liferay.dev/twentyfour#Demystifying%20Liferay%20Commerce%20Extensions) session
--- src/main/java/com/liferay/commerce/demo/order/entry/type/DemoCOREntryType.java
+++ src/main/java/com/liferay/commerce/demo/order/entry/type/DemoCOREntryType.java
@@ -1,7 +1,54 @@
 package com.liferay.commerce.demo.order.entry.type;
 
+import com.liferay.commerce.model.CommerceOrder;
+import com.liferay.commerce.order.rule.entry.type.COREntryType;
+import com.liferay.commerce.order.rule.model.COREntry;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import org.osgi.service.component.annotations.Component;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 /**
  * @author Jeff Handa
  */
-public class DemoCOREntryType {
+@Component(
+        immediate = true,
+        property = {
+                "commerce.order.rule.entry.type.key=" + DemoCOREntryType.KEY,
+                "commerce.order.rule.entry.type.order:Integer=10"
+        },
+        service = COREntryType.class
+)
+public class DemoCOREntryType implements COREntryType{
+
+    public static final String KEY = "demo-order-rule";
+
+    @Override
+    public boolean evaluate(COREntry corEntry, CommerceOrder commerceOrder) throws PortalException {
+        return false;
+    }
+
+    @Override
+    public String getErrorMessage(COREntry corEntry, CommerceOrder commerceOrder, Locale locale) throws PortalException {
+        ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+                "content.Language", locale, getClass());
+        return LanguageUtil.get(resourceBundle, "demo-order-rule-error-message");
+    }
+
+    @Override
+    public String getKey() {
+        return KEY;
+    }
+
+    @Override
+    public String getLabel(Locale locale) {
+        ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+                "content.Language", locale, getClass());
+
+        return LanguageUtil.get(resourceBundle, "demo-order-rule");
+    }
+
 }
